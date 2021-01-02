245fe46cdfbcf5ac5c4af1a67b3ea29a5cd488e4
william <191marquis@gmail.com>
2021-01-02 03:51:33 -0500

    [DEV] Extend color palette with theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // false was default, or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: {
+          2020: '#61dafb'
+        },
+        gray: {
+          2020: '#282c34'
+        },
+      },
+    },
   },
   variants: {
     extend: {},
