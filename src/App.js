1197ac00b1e96c96151ad8f26d7368b6f1d85d56
william <191marquis@gmail.com>
2021-01-02 03:54:16 -0500

    [DEV] Add placeholder widget

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Body from './components/Body';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Placeholder from './components/Placeholder';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Body />
+      <Placeholder classes="w-96 h-48"/>
       <Footer />
     </div>
   );
