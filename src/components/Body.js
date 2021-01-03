352c5fe11ffb75dc7bb815f75fee2b123ee78d2e
Gareth Brickman <brickstick@gmail.com>
2021-01-03 14:24:03 -0500

    [FIX] Remove unneeded content in body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,7 @@
-import logo from '../assets/snowflake_logo_color.svg';
-
 export default function Body() {
     return (
     <div className="Body">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>UnderReact</h1>
-        <p>Yes, I look ugly as frick... for now</p>
-        <p>My background color will be gray at the md breakpoint, but teal at every other breakpoint</p>
+        <h1>Body Content</h1>
     </div>
     )
 }
