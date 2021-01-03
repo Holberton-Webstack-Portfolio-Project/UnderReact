77b3f0766c568f262312f00194f9650d434cd354
william <191marquis@gmail.com>
2021-01-03 00:55:23 -0500

    [DEV] Style Navbar and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,18 @@ import { getFullYear, getFooterCopy } from '../utils/utils';
 export default function Footer () {
     return (
         <div className="
-        fixed 
-        bottom-0 
-        w-full p-5 
+        fixed
+        bottom-0
+        w-full p-5
         bg-teal-2020
-       dark:bg-gray-2020
+        dark:bg-gray-2020
         dark:text-white
         transition duration-450
+        grid
+        grid-col-1
         ">
-          <p>{`© Mighty Ducks ${getFullYear()}`}</p>
+          <p className="m-auto text-center">{`© Mighty Ducks 2020`}</p>
         </div>
     )
+    //Do we actually want the copyright year to update? ${getFullYear()}`}</p>
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,33 @@
+import React from 'react';
+import logo from '../assets/snowflake_logo_color.svg';
+import logo_gray from '../assets/snowflake_logo_gray.svg';
 import DarkModeButton from './DarkModeButton';
 
 export default function Nav() {
+    const darkMode =  window.document.documentElement.classList.contains("dark");
+
     return (
         <div className="
-        fixed 
-        top-0 
+        fixed
+        top-0
         bg-teal-2020
         dark:bg-gray-2020
-        w-full 
+        text-gray-2020
+        dark:text-teal-2020
+        w-full
+        h-24
         p-5
         transition duration-450
+        grid
+        grid-cols-3
         ">
-        <DarkModeButton />
+          <div>
+            <img src={(darkMode) ? logo : logo_gray} className="App-logo w-auto h-12 my-auto" alt="logo" />
+          </div>
+          <h1 className="m-auto text-3xl text-center">UnderReact</h1>
+          <div className="ml-auto my-auto">
+            <DarkModeButton />
+          </div>
         </div>
     )
 }
