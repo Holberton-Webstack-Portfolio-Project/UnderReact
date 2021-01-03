6c32a702d16121514ac1f746f13f27031c04bc2d
Gareth Brickman <brickstick@gmail.com>
2021-01-03 14:39:14 -0500

    [DEV] Use color logo until we figure out how to toggle it in dark mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,7 @@ import logo_gray from '../assets/snowflake_logo_gray.svg';
 import DarkModeButton from './DarkModeButton';
 
 export default function Nav() {
-    const darkMode =  window.document.documentElement.classList.contains("dark");
-
+/*     const darkMode =  window.document.documentElement.classList.contains("dark"); */
     return (
         <div className="
         fixed
@@ -22,7 +21,7 @@ export default function Nav() {
         grid-cols-3
         ">
           <div>
-            <img src={(darkMode) ? logo : logo_gray} className="App-logo w-auto h-12 my-auto" alt="logo" />
+          <img src={logo} className="App-logo w-auto h-12 my-auto" alt="logo" />
           </div>
           <h1 className="m-auto text-3xl text-center">UnderReact</h1>
           <div className="ml-auto my-auto">
