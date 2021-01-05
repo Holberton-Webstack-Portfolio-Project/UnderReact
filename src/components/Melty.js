601c7b24d966b4cd212f86d672bee9387452486e
william <191marquis@gmail.com>
2021-01-04 20:36:47 -0500

    [BUGFIX] Fix melty

---
+++ src/components/Melty.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ReactComponent as MeltyIcon } from '../assets/melty.svg';
+import './Melty.css';
+
+export default function Melty (props) {
+    return (
+        <MeltyIcon />
+    )
+}
