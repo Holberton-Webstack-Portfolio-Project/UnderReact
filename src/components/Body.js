b63e915126598f1530a74c6741c94f5701160d45
william <191marquis@gmail.com>
2021-01-04 20:58:10 -0500

    [BUGFIX] Fix footer and melty

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,15 @@
+import Melty from './Melty.js';
+
 export default function Body() {
     return (
      <div className="
+     my-24
      mx-auto
      grid 
      grid-flow-col 
      grid-rows-2 grid-cols-2 gap-4
      ">
+    <Melty />
     <div className="border-black border-2 mx-1">
         <p className="p-4">Panel 1</p>
       </div>
--- src/components/Melty.js
+++ src/components/Melty.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ReactComponent as MeltyIcon } from '../assets/melty.svg';
-import './Melty.css';
 
 export default function Melty (props) {
     return (
