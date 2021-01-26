82780833cc34a56756e6067fd156801d3afa2618
Gareth Brickman <brickstick@gmail.com>
2021-01-26 17:10:11 -0500

    [FIX] Remove unneeded imports and comments

--- src/components/SyntaxPane.js
+++ src/components/SyntaxPane.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {materialLight} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function SyntaxPane(props) {
   return (
