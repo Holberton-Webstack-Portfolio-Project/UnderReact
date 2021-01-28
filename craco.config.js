a5d96d31bbf3f3946654c60c990f38994ceafaf0
Gareth Brickman <brickstick@gmail.com>
2021-01-27 21:43:26 -0500

    [DEV] Add build optimization tools

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,26 @@
 // craco.config.js
+const CracoAntDesignPlugin = require("craco-antd");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 module.exports = {
+  webpack: {
+    plugins: [
+      new BundleAnalyzerPlugin(),
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp(
+          '\\.(' +
+          ['js', 'css'].join('|') +
+          ')$'
+        ),
+        threshold: 1024,
+        minRatio: 0.8
+      }),
+    ]
+  },
+  plugins: [{ plugin: CracoAntDesignPlugin }],
   style: {
     postcss: {
       plugins: [
